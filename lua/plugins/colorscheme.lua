df70beef261f0b1b2f4c4c897ae50d1b00e6b620
Gabriel Henrique Amazonas <gabriel.hamazonas@gmail.com>
2026-01-22 17:18:24 -0300

    Ajustes comentario // e tema nord oficial

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,13 +13,28 @@
 ]]
 
 return {
+    -- {
+    --     "arcticicestudio/nord-vim",
+    --     lazy = true,
+    --     config = function()
+    --         vim.g.nord_borders = true -- Habilita bordas, se desejar
+    --         vim.g.nord_contrast = true -- Aumenta o contraste, se desejar
+    --         vim.g.nord_disable_background = true
+    --     end,
+    -- },
     {
-        "arcticicestudio/nord-vim",
-        lazy = true,
-        config = function()
-            vim.g.nord_borders = true -- Habilita bordas, se desejar
-            vim.g.nord_contrast = true -- Aumenta o contraste, se desejar
-            vim.g.nord_disable_background = true
+        "gbprod/nord.nvim",
+        -- ou "shaunsingh/nord.nvim" (muito similar)
+        lazy = false, -- carrega imediatamente
+        priority = 1000, -- carrega antes de tudo
+        opts = {
+            -- aqui você pode customizar se quiser
+            transparent = false, -- ou true se quiser fundo transparente
+            errors = { mode = "none" }, -- ou "background" / "undercurl"
+        },
+        config = function(_, opts)
+            require("nord").setup(opts)
+            -- não precisa chamar vim.cmd.colorscheme aqui
         end,
     },
     {
